5f0baa2cb96e617298f8cab9876287476489cee1
GABRIEL_SZM <gabriel_szm@outlook.com>
2016-07-31 22:12:29 -0500

    calculadora

---
+++ app/src/main/java/com/facci/calculadoragz/MainActivityGZ.java
@@ -0,0 +1,24 @@
+package com.facci.calculadoragz;
+
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivityGZ extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_gz);
+
+    }
+
+
+
+
+}
